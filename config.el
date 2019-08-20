5bd57e48d142faf3f56b5e2a7c425dda2f94df14
Tom Passarelli <tom.passarelli@protonmail.com>
2019-08-19 21:06:25 -0400

    org-mode-agena added

--- config.el
+++ config.el
@@ -1,2 +1,21 @@
 (setq
- projectile-project-search-path '("~/V/")) 
+ projectile-project-search-path '("~/V/"))
+
+(def-package! org-super-agenda
+  :after org agenda
+  :init
+  (setq org-super-agenda-groups '((:name "Today"
+                                  :time-grid t
+                                  :scheduled today)
+                           (:name "Due today"
+                                  :deadline today)
+                           (:name "Important"
+                                  :priority "A")
+                           (:name "Overdue"
+                                  :deadline past)
+                           (:name "Due soon"
+                                  :deadline future)
+                           (:name "Big Outcomes"
+                                  :tag "bo")))
+  :config
+  (org-super-agenda-mode))
